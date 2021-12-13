b380df3fbda73cf2b5c5f3c9cda039e7e1f4848a
ildirim <ildirim.huseyn@gmail.com>
2021-12-13 17:50:37 +0400

    edit

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -165,7 +165,7 @@ class UserService
 
 			$this->user->where('id', $storedUser->id)->update($request);
 
-        	// Event::dispatch(new RegisterMailEvent($storedUser->id, $password));
+        	Event::dispatch(new RegisterMailEvent($storedUser->id, $password));
 
         	return $storedUser->id;
 		}
